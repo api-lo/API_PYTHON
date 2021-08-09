3b1aee5642dade65a22cb7c5faadf1e9cd6f9f7d
Dayi12 <69596521+Dayi12@users.noreply.github.com>
2021-08-08 19:56:54 -0500

    API arreglos

--- NewProyect/app.py
+++ NewProyect/app.py
@@ -8,6 +8,7 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['CORS_HEADERS'] = 'Content-Type'
 from users import users
+from doctores import doctores
 
 @cross_origin()
 
@@ -20,23 +21,26 @@ def test():
 def getUsers():
     return jsonify(users)
 
-@app.route('/users/<string:idUsuario>')
-def getUser(idUsuario):
-    retorno = [user for user in users if user['idUsuario']== idUsuario]
+@app.route('/users/<string:Correo>')
+def getUser(Correo):
+    retorno = [user for user in users if user['Correo']== Correo]
     return jsonify(retorno)
 
-@app.route('/users', methods=['POST'])
-def addUser():
-    newUser = {
-         'idUsuario': request.json['idUsuario'], 
+@app.route('/clave/<string:contrasenia>')
+def getContrasenia(contrasenia):
+    retorno = [user for user in users if user['contrasenia']== contrasenia]
+    return jsonify(retorno)
+
+@app.route('/doctores', methods=['POST'])
+def addDoctor():
+    newDoctor = {
+         'idDoctor': request.json['idDoctor'], 
          'Nombre': request.json['Nombre'], 
-         'Genero': request.json['Genero'],
-         'Telefono': request.json['Telefono'],
-         'contrasenia': request.json['contrasenia'],
-         'Correo': request.json['Correo'],
+         'contacto': request.json['contacto'],
+         'Especialidad': request.json['Especialidad']
     }
-    users.append(newUser)
-    return jsonify(users)
+    doctores.append(newDoctor)
+    return jsonify(doctores)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
